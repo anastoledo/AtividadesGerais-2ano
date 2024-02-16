0dabf75bb66dbc231ac892b01cd58c451be093ec
Ana Júlia Toledo <134728792+anastoledo@users.noreply.github.com>
2024-02-16 13:45:06 -0300

    Add files via upload

---
+++ Orientação_a_Objetos/1Bimestre/aula2_ex1.php
@@ -0,0 +1,12 @@
+<?php
+
+for ($i=0; $i < 10; $i++) { 
+$numero = readline("Informe 10 números! ");
+
+    if ($numero > 0) {
+        print "$numero" . "é positivo!\n ";
+    }
+    else {
+        print "$numero" . " é negativo!\n ";
+    }
+}
