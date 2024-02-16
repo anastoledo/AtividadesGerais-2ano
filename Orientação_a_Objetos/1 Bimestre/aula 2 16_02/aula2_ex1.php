6fab0993649245f2c7de3633209be5f200205e2a
Ana Júlia Toledo <134728792+anastoledo@users.noreply.github.com>
2024-02-16 14:35:49 -0300

    Update aula2_ex1.php

--- Orientação_a_Objetos/1 Bimestre/aula 2 16_02/aula2_ex1.php
+++ Orientação_a_Objetos/1 Bimestre/aula 2 16_02/aula2_ex1.php
@@ -4,14 +4,10 @@ for ($i=0; $i < 10; $i++) {
 
     $numero = readline("Informe 10 números! ");
 
-    if ($numero > 0) {
+    if ($numero >= 0) {
         print "$numero" . " é positivo!\n ";
     }
-    else if ($numero < 0) {
-        print "$numero" . " é negativo!\n ";
-    }
     else {
-        # code...
-    }
-    
+        print "$numero" . " é negativo!\n ";
+    }   
 }
