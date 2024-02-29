9165a585ccfd8c03a43574ae889b72eecc97a30a
Ana Júlia Toledo <134728792+anastoledo@users.noreply.github.com>
2024-02-29 15:44:23 -0300

    Add files via upload

---
+++ Orientação_a_Objetos/1 Bimestre/aula 4 (29_02)/ex2_array.php
@@ -0,0 +1,18 @@
+<?php
+    
+$vetor1 = array();
+$vetor2 = array();
+
+for ($i=0; $i < 5; $i++) { 
+    $respostaUser = readline("Informe uma palavra para adicionar no array: ");
+    $vetor1[] = $respostaUser;
+    print "\n";
+}
+
+array_push ($vetor2, $vetor1);
+
+foreach ($vetor2 as $vetor) {
+    
+    print $vetor .  ",";
+}
+
