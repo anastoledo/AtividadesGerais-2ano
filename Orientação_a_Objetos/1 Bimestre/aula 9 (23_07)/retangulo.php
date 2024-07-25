758c594a3f292de8992c3da206e567d13e21d9bd
Ana Júlia Toledo <134728792+anastoledo@users.noreply.github.com>
2024-07-25 14:45:41 -0300

    Update retangulo.php

--- Orientação_a_Objetos/1 Bimestre/aula 9 (23_07)/retangulo.php
+++ Orientação_a_Objetos/1 Bimestre/aula 9 (23_07)/retangulo.php
@@ -9,40 +9,29 @@ class Retangulo {
     //método
     function area () {
         $area = $this->base*$this->altura;
-        print "A área do retângulo é $area\n";
+        return $area;
+
     }
     function perimetro () {
         $perimetro = 2*($this->base+$this->altura);
-        print "Já o perimetro é $perimetro\n";
+        return $perimetro;
     }
 
 }
 
 //programa principal
-$retangulo1 = new Retangulo();
 
-$retangulo1->base = readline ("Informe a base: \n");
-$retangulo1->altura = readline ("Informe a altura: \n");
+for ($i=0; $i < 3 ; $i++) { 
+    $retangulo = new Retangulo();
 
-$retangulo1-> area();
-$retangulo1-> perimetro();
+    $retangulo->base = readline ("Informe a base: \n");
+    $retangulo->altura = readline ("Informe a altura: \n");
 
-print "*********************\n";
+    $retangulo-> area();
+    print "A área do retangulo $i é igual a ". $retangulo-> area() . " cm \n";
+    $retangulo-> perimetro();
+    print "O perimetro do retangulo $i é igual a ". $retangulo-> perimetro() . " cm \n";
 
-$retangulo2 = new Retangulo();
+    print "*********************\n";
+}
 
-$retangulo2->base = readline ("Informe a base: \n");
-$retangulo2->altura = readline ("Informe a altura: \n");
-
-$retangulo2-> area();
-$retangulo2-> perimetro();
-
-print "*********************\n";
-
-$retangulo3 = new Retangulo();
-
-$retangulo3->base = readline ("Informe a base: \n");
-$retangulo3->altura = readline ("Informe a altura: \n");
-
-$retangulo3-> area();
-$retangulo3-> perimetro();
