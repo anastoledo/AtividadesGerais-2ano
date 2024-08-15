39fad0a78f9afb20cb0d23bd63fc05770b9637c7
Ana Júlia Toledo <ana.toledo.foz@gmail.com>
2024-08-14 23:22:31 -0300

    aaaaa

--- Orientação_a_Objetos/2 Bimestre/aula 12 (15_08)/calculadora.php
+++ Orientação_a_Objetos/2 Bimestre/aula 12 (15_08)/calculadora.php
@@ -54,8 +54,8 @@ $calculadora = new Calculadora();
 $calculadora->setNumA(readline("Informe o valor de A: "));
 $calculadora->setNumB(readline("Informe o valor de B: "));
 
-print "A soma de A + B é " . $calculadora->soma();
-print "A subtração de A - B é " . $calculadora->subtracao();
-print "A multiplicação de A X B é " . $calculadora->multiplicacao();
-print "A divisão de A / B é " . $calculadora->divisao();
-print "O resto de A e B é " . $calculadora->resto();
+print " A soma de A + B é " . $calculadora->soma(). "\n ";
+print " A subtração de A - B é " . $calculadora->subtracao(). "\n ";
+print " A multiplicação de A X B é " . $calculadora->multiplicacao(). "\n ";
+print " A divisão de A / B é " . $calculadora->divisao(). "\n ";
+print " O resto de A e B é " . $calculadora->resto(). "\n ";
--- Orientação_a_Objetos/2 Bimestre/aula 12 (15_08)/viagem.php
+++ Orientação_a_Objetos/2 Bimestre/aula 12 (15_08)/viagem.php
@@ -4,9 +4,9 @@ class Veiculo {
     private $capacidade;
     private $passageiros;
 
-    public function venderPassagem($quantidade) {
-        if ($this->passageiros + $quantidade <= $this->capacidade) {
-            $this->passageiros = $this->passageiros + $quantidade;
+    public function venderPassagem($quantidadePassagens) {
+        if ($this->passageiros + $quantidadePassagens <= $this->capacidade) {
+            $this->passageiros = $this->passageiros + $quantidadePassagens;
             return true;
         } else {
             return false;
@@ -37,3 +37,21 @@ class Veiculo {
 
 // Programa principal
 
+$veiculo = new Veiculo();
+$veiculo->setCapacidade(readline("Informe a capacidade do veiculo: "));
+
+while (true) {
+    $quantidadePassagens = readline("Informe a quantidade de passagens: (DIGITE 0 PARA SAIR ");
+
+    if ($quantidadePassagens == 0) {
+        print "Encerrando operações..\n";
+        break;
+    }
+    if ($veiculo->venderPassagem($quantidadePassagens)) {
+        "Parabénss, a sua passagem foi comprada com SUCESSO! O total de passageiros no veiculo é de " . $veiculo->getPassageiros().  "\n";
+    }
+    else {
+        "Não fui possivel efetuar a venda da passagem, pois atingimos 100% da capacidade do veículo\n";
+        "No momento o número de passageiros no veículo é de " . $veiculo->getPassageiros() . "mas a nossa capacidade no momento é de " . $veiculo->getCapacidade(). "\n";
+    }
+}
