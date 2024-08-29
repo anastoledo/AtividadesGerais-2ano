b8b97127d4643cf95f07995b201f607a0b0f6a9d
Ana Júlia Toledo <134728792+anastoledo@users.noreply.github.com>
2024-08-28 21:32:15 -0300

    Create alunos.php

---
+++ Orientação_a_Objetos/2 Bimestre/aula 14 (29_08)/alunos.php
@@ -0,0 +1,54 @@
+<?php
+class Escola {
+    private $nome;
+    private $endereco;
+    private $quantidade_alunos;
+
+    // GETs e SETs
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function getEndereco() {
+        return $this->endereco;
+    }
+
+    public function setEndereco($endereco) {
+        $this->endereco = $endereco;
+    }
+
+    public function getQuantidadeAlunos() {
+        return $this->quantidade_alunos;
+    }
+
+    public function setQuantidadeAlunos ($quantidade_alunos) {
+        $this->quantidade_alunos = $quantidade_alunos;
+    }
+
+    //passar pra string
+    public function __toString() {
+        return "Nome: " . $this->getNome() . " | Endereço: " .$this->getEndereco(). " | Quantidade de Alunos: " . $this->getQuantidadeAlunos()  . "\n";
+    }
+}
+
+// programa principal
+$escolas = array();
+
+for ($i = 0; $i < 4; $i++) {
+  $escola = new Escola ();
+  $escola->setNome(readline("Informe o nome da escola " . $i + 1 . ": " ));
+  $escola->setEndereco(readline("Informe o endereço da escola " . $i + 1 . ": " ));
+  $escola->setQuantidadeAlunos(readline("Informe a quantidade de alunos da escola " . $i + 1 . ": " ));
+
+  array_push($escolas, $escola);
+  
+}
+
+//imprimir o array
+foreach ($escolas as $escola) {
+    echo $escola;
+}
